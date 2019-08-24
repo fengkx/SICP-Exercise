b0d118b8f6be6cee35197b1ebb7fa95e15137851
fengkx <liangkx8237@gmail.com>
2019-08-24 11:25:04 +0800

    fix: comment out a good_enough?

--- ex1.8.scm
+++ ex1.8.scm
@@ -4,8 +4,8 @@
 (define (average x y) (/ (+ x y) 2))
 (define (abs x) (if (< x 0) (- 0 x) x))
 
-(define (good_enough? guess x)
-  (< (abs (- (cube guess) x)) 0.001))
+;(define (good_enough? guess x)
+;  (< (abs (- (cube guess) x)) 0.001))
 
 (define (good_enough? guess x)
   (< (abs (-(improve guess x) guess)) 0.001))
@@ -19,4 +19,4 @@
       (curt_iter (improve guess x) x)))
 (define (curt x)
   (curt_iter 1 x))
-(curt 8)
+(curt 64)
